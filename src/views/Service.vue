611b3198241fbc4473f64ebecff91c76fcd3af84
imac ssd <imacssd@iMac-di-imac.homenet.telecomitalia.it>
2022-12-09 06:33:39 +0100

    push di venerdi

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,6 +1,7 @@
 <template>
     <div class=" flex justify-center  items-center m-auto text-8xl capitalize">
-      My work 2
+      My work 2 
+      push di venerdi 9 dicembre
    
     </div>
     <modale class="flex justify-center"/>
